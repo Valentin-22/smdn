59113bebd1ee43340037c17278ed6956ce85fe0b
Valentin-22 <85140362+Valentin-22@users.noreply.github.com>
2024-07-11 12:15:21 -0300

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const links = document.querySelectorAll('.sidebar-menu a');
+    links.forEach(link => {
+        link.addEventListener('click', function(event) {
+            event.preventDefault();
+            const targetId = link.getAttribute('href').substring(1);
+            const targetElement = document.getElementById(targetId);
+            window.scrollTo({
+                top: targetElement.offsetTop,
+                behavior: 'smooth'
+            });
+        });
+    });
+});
